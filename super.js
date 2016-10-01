4d9b381269730755d14ca5e6355f9186523e15b7
Vaseem Abbas Mohammed <vaseemabbas.mohammed@gmail.com>
2016-10-02 02:49:29 +0530

    Vaseem: Iterator fixes, Array concat and Object entries methods added

--- super.js
+++ super.js
@@ -1,14 +1,38 @@
 /**
  * Created by vabbasmoha on 10/2/16.
  */
-Array.prototype.iterator = function iterator(){
-    int pos = 0;
-    this.hasNext(){
+var superjs = Object.create({}, {});
+
+Array.prototype.iterator = function(){
+    var pos = 0;
+    this.hasNext = function(){
         return this.length > pos;
-    }
-    this.next(){
-        return this[pos];
-    }
-    return obj;
+    };
+    this.next = function(){
+        return this[pos++];
+    };
+    return this;
 };
 
+Array.prototype.concat = function concat(a){
+    a.iterator();
+    while(a.hasNext()){
+        this.push(a.next());
+    }
+};
+
+
+Object.prototype.entries = function entries(){
+    var keys = Object.keys(this), i=0, entries= [];
+    while(i < keys.length){
+        var entry = [];
+        entry.push(keys[i]);
+        entry.push(this[keys[i++]]);
+        entries.push(entry);
+    }
+    return entries;
+};
+
+Object.prototype.toHTML = function toHTML(){
+
+}
