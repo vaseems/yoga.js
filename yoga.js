c0ce5003cc0cb714a370ecf119b43edc10196fdf
Vaseem Abbas Mohammed <vaseemabbas.mohammed@gmail.com>
2016-10-09 01:22:22 +0530

    apply() function to Array. Apply any function to each element of an array and return new array

--- yoga.js
+++ yoga.js
@@ -14,14 +14,23 @@ Array.prototype.iterator = function(){
     return this;
 };
 
-Array.prototype.concat = function concat(a){
+Array.prototype.concat = function(a){
     a.iterator();
     while(a.hasNext()){
         this.push(a.next());
     }
 };
 
-Object.prototype.entries = function entries(){
+Array.prototype.apply = function(operation){
+    this.iterator();
+    var result = [];
+    while(this.hasNext()){
+        result.push(operation.call(this, this.next()));
+    }
+    return result;
+}
+
+Object.prototype.entries = function(){
     var keys = Object.keys(this), i=0, entries= [];
     while(i < keys.length){
         var entry = [];
