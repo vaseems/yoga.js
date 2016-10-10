4cb3bb1cabc1dd88b784b8dcf64d2ccf79c5bae9
Vaseem Abbas Mohammed <vaseemabbas.mohammed@gmail.com>
2016-10-10 17:51:38 +0530

    Added isArray, isObject in WINDOW, memoize in FUNCTION, difference, truthy in Array

--- yoga.js
+++ yoga.js
@@ -1,16 +1,41 @@
 /**
- * Created by vabbasmoha on 10/2/16.
+ * @license
+ * yoga.js <https://yogajs.com/>
+ * Released under MIT license <https://yogajs.com/license>
+ * Copyright Vaseem Abbas Mohammed
  */
-(function(){
+
+;(function(){
 
     var yoga = {
-        ananthasana: {type: Array},
-        salabhasana: {type: String},
-        nadishodhana: {type: Number},
-        balasana: {type: Boolean},
-        dhyana: {type: Date},
-        ekasana: {type: Object}
+        ananthasana: {prototype: Array.prototype},
+        salabhasana: {prototype: String.prototype},
+        nadishodhana: {prototype: Number.prototype},
+        balasana: {prototype: Boolean.prototype},
+        dhyana: {prototype: Date.prototype},
+        ekasana: {prototype: Object.prototype},
+        matsyasana: {prototype: Function.prototype},
+        virabhadrasana: {prototype: Window.prototype}
     };
+    var MSG_NO_DIFFERENCE = "No Difference";
+
+    var isArray = function(array){
+      return typeof array == 'object' && (array) instanceof Array && !((array) instanceof Object);
+    };
+
+    var isObject = function(obj){
+        return typeof array == 'object' && (array) instanceof Array && !((array) instanceof Object);
+    };
+
+    var contains = function(array, ele){
+      return array.indexOf(ele) != -1;
+    };
+
+    yoga.ananthasana.contains = function(ele){
+        return contains(this, ele);
+    };
+
+
 
     yoga.ananthasana.iterator = function(){
         var pos = 0;
@@ -24,10 +49,7 @@
     };
 
     yoga.ananthasana.concat = function(a){
-        a.iterator();
-        while(a.hasNext()){
-            this.push(a.next());
-        }
+        this.push.apply(this, a);
     };
 
     yoga.ananthasana.apply = function(operation){
@@ -39,12 +61,47 @@
         return result;
     };
 
+    yoga.ananthasana.truthy = function(){
+        var truths = [], falsy = [];
+        this.iterator();
+        while(this.hasNext()){
+            var next = this.next();
+            if(next){
+                truths.push(next);
+            }else{
+                falsy.push(next);
+            }
+        }
+        console.log(falsy);
+        return truths;
+    };
+
+    yoga.ananthasana.difference = function(array){
+        var difference = [];
+        array.forEach(function(ele){
+           contains(this, ele) && difference.push(ele);
+        });
+
+        this.forEach(function(ele){
+            contains(array, ele) && difference.push(ele);
+        });
+        return difference.length == 0?MSG_NO_DIFFERENCE:difference;
+    };
+
+    yoga.ananthasana.differenceBy = function(array, operationBy){
+        var difference = [];
+        this.forEach(function(ele){
+            contains(array, ele) && difference.push(ele);
+        });
+        return difference.length == 0?MSG_NO_DIFFERENCE:difference;
+    };
+
     yoga.ekasana.size = function(){
-      return Object.keys(this).length;
+        return Object.keys(this).length;
     };
 
     yoga.ekasana.entries = function(){
-        var keys = Object.keys(this), i=0, entries= [];
+        var keys = Object.keys(this), i = 0, entries = [];
         while(i < keys.length){
             var entry = [];
             entry.push(keys[i]);
@@ -54,9 +111,31 @@
         return entries;
     };
 
+    yoga.matsyasana.memoize = function(){
+            var memo = {};
+            var slice = Array.prototype.slice;
+            var self = this;
+            return function() {
+                var args = slice.call(arguments);
+
+                if (args in memo)
+                    return memo[args];
+                else
+                    return (memo[args] = self.apply(this, args));
+            }
+    };
+
+    yoga.virabhadrasana.isArray = function(obj){
+        return isArray(obj);
+    };
+
+    yoga.virabhadrasana.isObject = function(obj){
+        return isObject(obj);
+    }
+
     Object.keys(yoga).forEach(function(asana){
         Object.keys(yoga[asana]).forEach(function(power){
-            yoga[asana].type.prototype[power] = yoga[asana][power];
+            yoga[asana].prototype[power] = yoga[asana][power];
         });
     });
 })();
